8b35666c6118b25f23ea0a4a54b5e37963fa21b1
Theodoros Ntakouris <zarkopafilis@gmail.com>
2017-04-28 15:56:03 +0300

    Streams

---
+++ src/eight/Streams.java
@@ -0,0 +1,73 @@
+package eight;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by zarkopafilis on 4/28/2017.
+ */
+public class Streams {
+
+	public static void main(String[] args) {
+
+		Random r = new Random();
+		String[] friendlyNames = {"Alpha", "Beta", "Delta", "George", "Takis"};
+
+		List<Transaction> transactions = new ArrayList<>();
+		for(int i=0;i<50;i++) {
+			transactions.add(new Transaction(r.nextBoolean() ? TransactionType.BANK : TransactionType.PAYPAL,
+					r.nextInt(500) + 500,
+					friendlyNames[r.nextInt(friendlyNames.length)]));
+		}
+
+		transactions.stream().filter(x -> x.getMoney() > 750).findFirst().ifPresent(System.out::println);
+
+		long startTime = System.currentTimeMillis();
+		System.out.println("Sum of transaction money: " + transactions.stream().mapToInt(x -> x.getMoney()).sum());
+		System.out.println("Time: " + (System.currentTimeMillis() - startTime));
+
+		startTime = System.currentTimeMillis();
+		System.out.println("Sum of transaction money: " + transactions.parallelStream().mapToInt(x -> x.getMoney()).sum());
+		System.out.println("Time: " + (System.currentTimeMillis() - startTime));
+	}
+
+}
+
+enum TransactionType{
+	BANK,PAYPAL
+}
+
+class Transaction{
+
+	TransactionType transactionType;
+	int money;
+	String friendlyName;
+
+	public Transaction(TransactionType transactionType, int money, String friendlyName) {
+		this.transactionType = transactionType;
+		this.money = money;
+		this.friendlyName = friendlyName;
+	}
+
+	public TransactionType getTransactionType() {
+		return transactionType;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public String getFriendlyName() {
+		return friendlyName;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction{" +
+				"transactionType=" + transactionType +
+				", money=" + money +
+				", friendlyName='" + friendlyName + '\'' +
+				'}';
+	}
+}
